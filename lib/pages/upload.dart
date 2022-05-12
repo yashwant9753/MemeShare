86f4ea157da27ea6a3c358f2501b37cd53864748
yashwant9753 <ys25701@gmail.com>
2022-05-12 18:59:13 +0530

    upload form

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:socialnetwork/models/user.dart';
 import 'package:socialnetwork/networkColors.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'dart:core';
+
+import 'package:socialnetwork/widgets/progress.dart';
 
 class Upload extends StatefulWidget {
+  final User currentUser;
+
+  Upload({this.currentUser});
+
   @override
   _UploadState createState() => _UploadState();
 }
 
 class _UploadState extends State<Upload> {
   XFile file;
+  bool isUploading = false;
 
   handleTakePhoto() async {
     Navigator.pop(context);
@@ -33,23 +44,24 @@ class _UploadState extends State<Upload> {
 
   selectImage(parentContext) {
     return showDialog(
-        context: parentContext,
-        builder: (context) {
-          return SimpleDialog(
-            title: Text("Create Post"),
-            children: <Widget>[
-              SimpleDialogOption(
-                  child: Text("Photo with Camera"), onPressed: handleTakePhoto),
-              SimpleDialogOption(
-                  child: Text("Image from Gallery"),
-                  onPressed: handleChooseFromGallery),
-              SimpleDialogOption(
-                child: Text("Cancel"),
-                onPressed: () => Navigator.pop(context),
-              )
-            ],
-          );
-        });
+      context: parentContext,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text("Create Post"),
+          children: <Widget>[
+            SimpleDialogOption(
+                child: Text("Photo with Camera"), onPressed: handleTakePhoto),
+            SimpleDialogOption(
+                child: Text("Image from Gallery"),
+                onPressed: handleChooseFromGallery),
+            SimpleDialogOption(
+              child: Text("Cancel"),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        );
+      },
+    );
   }
 
   Container buildSplashScreen() {
@@ -80,8 +92,127 @@ class _UploadState extends State<Upload> {
     );
   }
 
-  buildUploadForm() {
-    return Text("File loaded");
+  clearImage() {
+    setState(() {
+      file = null;
+    });
+  }
+
+  handleSubmitt() {
+    setState(() {
+      isUploading = true;
+    });
+  }
+
+  Scaffold buildUploadForm() {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: clearImage),
+        title: Text(
+          "Caption Post",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                "Post",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+              color: NetworkColors.colorDarkBlue,
+              onPressed: isUploading ? null : () => handleSubmitt(),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          isUploading ? linearProgress() : Text(""),
+          Container(
+            height: 220.0,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: FileImage(File(file.path)),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(widget.currentUser.photoUrl),
+            ),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Write a caption...",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(
+              Icons.pin_drop,
+              color: Colors.orange,
+              size: 35.0,
+            ),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Where was this photo taken?",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 200.0,
+            height: 100.0,
+            alignment: Alignment.center,
+            child: RaisedButton.icon(
+              label: Text(
+                "Use Current Location",
+                style: TextStyle(color: Colors.white),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Colors.blue,
+              onPressed: () => print('get user location'),
+              icon: Icon(
+                Icons.my_location,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -90,4 +221,24 @@ class _UploadState extends State<Upload> {
   }
 }
 
-//  Image.asset("images/upload.gif"),
+ // handleChooseFromGallery() async {
+  //   Navigator.pop(context);
+  //   // XFile file = await ImagePicker().pickImage(source: ImageSource.gallery);
+  //   // setState(() {
+  //   //   this.file = file;
+  //   // });
+  //   try {
+  //     final result = await FilePicker.platform.pickFiles();
+
+  //     // open single file
+  //     if (result != null) {
+  //       setState(() {
+  //         image = File(result.files.first.path.toString());
+
+  //         print(image.toString());
+  //       });
+  //     }
+  //   } on PlatformException catch (e) {
+  //     print("Something is Wrong : $e");
+  //   }
+  // }
